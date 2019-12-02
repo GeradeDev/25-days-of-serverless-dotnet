648fead9b7053b1080b8d7fb0e4c9d7b63e72111
Scott Rudy <scottrudy@yahoo.com>
2019-12-01 20:17:09 -0500

    Make safer

--- day01/dreidel.cs
+++ day01/dreidel.cs
@@ -7,12 +7,19 @@ using Microsoft.AspNetCore.Http;
 namespace day01 {
     public static class dreidel     {
         private static readonly Random _random = new Random();
+        private static object _lock  = new object();
         private static readonly  string[] sides = new string[] { "נ (nun)", "ג (gimel)", "ה (hei)", "ש (shin)" };
 
         [FunctionName("dreidel")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "spin")] HttpRequest req)  {
-            return (ActionResult)new OkObjectResult($"{sides[_random.Next(sides.Length)]}");
+            return (ActionResult)new OkObjectResult($"{sides[GetRandomValue(sides.Length)]}");
+        }
+
+        private static int GetRandomValue(int max) {
+            lock (_lock) {
+                return _random.Next(max);
+            }
         }
     }
 }
